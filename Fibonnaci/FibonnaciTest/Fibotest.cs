36e8ba7302287a33628bd56ddfbba6cf547dcdff
Ricardo Yukio Ide <ryiskate@yahoo.com.br>
2011-04-10 01:18:41 -0300

    Fibonnaci test trivial result for 0

---
+++ Fibonnaci/FibonnaciTest/Fibotest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FibonnaciTest
+{
+    [TestFixture]
+    public class Fibotest 
+    {
+        [Test]
+        public void fibonnaci_0_returns_0()
+        {
+            var result = Fibonnaci.Calc(0);
+            Assert.AreEqual(0, result);
+        }
+    }
+
+    public class Fibonnaci
+    {
+
+        public static int Calc(int value)
+        {
+            return 0;
+        }
+    }
+}
